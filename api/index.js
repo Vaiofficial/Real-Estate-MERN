3f1be579487b2c7a493d11eb7c72829ec7819a6e
P Vaibhav <pvaibhavofficial@gmail.com>
2023-12-06 20:28:56 +0530

    created middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -23,3 +23,15 @@ app.listen(3000 , ()=>{
 //so use isme /api/test mai jyga to userRouter use kro
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
+
+//MIDDLEWARE
+app.use((err , req , res , next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
