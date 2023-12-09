129d9ca9a6d4cb68b0ebe2241ba893a53beff407
P Vaibhav <pvaibhavofficial@gmail.com>
2023-12-09 10:48:31 +0530

    completed signup page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,32 +1,81 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  //spread operator ka use kar rhe hai , jab user let username field mai kuch likha and dusre field mai likhega to dusre field ka data hatega nahi.
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    //preventdefault se loading effect handle karrhe h , signup click karne par jo hota hai.
+    e.preventDefault();
+
+    //errors & loading effect button mai , unko handle kar rhe hai.
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      //successfully signup hua to login page par redirect kar dere hai.
+      navigate("/sign-in");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="font-semibold text-center my-7 text-3xl">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="username"
           className="border rounded-lg p-3 focus:outline-none"
           id="username"
+          onChange={handleChange}
         />
         <input
           type="text"
           placeholder="email"
           className="border rounded-lg p-3 focus:outline-none"
-          id="username"
+          id="email"
+          onChange={handleChange}
         />
         <input
           type="text"
           placeholder="password"
           className="border rounded-lg p-3 focus:outline-none"
-          id="username"
+          id="password"
+          onChange={handleChange}
         />
         {/* Signup button */}
-        <button className="bg-slate-700 text-white uppercase rounded-lg p-3 font-semibold hover:opacity-95 disabled:opacity-80">
-          Sign up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white uppercase rounded-lg p-3 font-semibold hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
 
@@ -34,9 +83,10 @@ export default function SignUp() {
       <div className="flex gap-2 mt-5">
         <p>Have an account ?</p>
         <Link to={"/sign-in"}>
-        <span className="text-red-600">Sign in</span>
+          <span className="text-red-600">Sign in</span>
         </Link>
       </div>
+      {error && <p className="text-red-600 mt-5">{error}</p>}
     </div>
   );
 }
