fc7258b275c17cd77e644f54364252c8734d6423
P Vaibhav <pvaibhavofficial@gmail.com>
2023-12-09 12:09:47 +0530

    created signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import Jwt from "jsonwebtoken";
 
-export const signup = async (req, res , next) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -15,6 +16,25 @@ export const signup = async (req, res , next) => {
   }
 };
 
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const ValidPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!ValidPassword) return next(errorHandler(401, "Wrong Credentials"));
+    const token = Jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    //password ko even hash form mai bhi nahi bhejna hai so ...rest ko bhej rhe except password
+    const {password: pass , ...rest} = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
 //1 . now we use the model that we created to save this information.
 
 //2 .password ko encrypt bhi karna hoga (so here we are hashing it , here 10 is salt number - hash number which will combine with password to encypt it)
@@ -24,3 +44,7 @@ export const signup = async (req, res , next) => {
 //5 . we dont want to show error in console we will send it to the user , thats why using try  & catch.
 
 //6.best way to handle the error is by using middleware & function. This way is not a good approach.
+
+//7. jwt ka use kar rhe hai yaha data ko client and server ke beech securely transfer ke liye.
+
+//8. httpOnly ke karan koi third party ye cookie ko access nahi kar skta h.
