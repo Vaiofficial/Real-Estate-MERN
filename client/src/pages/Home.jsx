3b0314fb862f0abd0ecd041d823b4ce7b056cb20
P Vaibhav <pvaibhavofficial@gmail.com>
2023-12-16 20:06:54 +0530

    Completed Home Page UI

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,172 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import SwiperCore from "swiper";
+import "swiper/css/bundle";
+import ListingItem from "../components/ListingItem";
 
 export default function Home() {
+  const [offerListings, setOfferListings] = useState([]);
+  const [saleListings, setSaleListings] = useState([]);
+  const [rentListings, setRentListings] = useState([]);
+  SwiperCore.use([Navigation]);
+  console.log(offerListings);
+
+  // 3 section bnaye hai home page mai , jisme images show karne hai , first offers , second rent and third sale.
+  //ak sath teeno call ni karenge , ak use effect se phle ak aur jaise hi vo use effect success hua , agla use effect ussi ke andar call kar denge and simillary for the last one.
+  useEffect(() => {
+    //define fetchofferlistings fucntion
+    const fetchOfferListings = async () => {
+      try {
+        const res = await fetch("/api/listing/get?offer=true&limit=4");
+        const data = await res.json();
+        setOfferListings(data);
+        //phla vala clear hua phir call kaare rent vala function.
+        fetchRentListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    //define rentlisting function.
+    const fetchRentListings = async () => {
+      try {
+        const res = await fetch("/api/listing/get?type=rent&limit=4");
+        const data = await res.json();
+        setRentListings(data);
+        //calling third function that is sale listing.
+        fetchSaleListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    //define fetchsalelistings  function here.
+    const fetchSaleListings = async () => {
+      try {
+        const res = await fetch("/api/listing/get?type=sale&limit=4");
+        const data = await res.json();
+        setSaleListings(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchOfferListings();
+  }, []);
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      {/* top */}
+      <div className="flex flex-col gap-6 p-24 px-3 max-w-6xl mx-auto">
+        <h1 className="text-slate-700 font-bold text-2xl sm:text-4xl md:text-6xl ">
+          Your Space, Your Style:{" "}
+          <span className="text-slate-600">Rao Resident</span>
+          <br />
+          Your Home Solution
+        </h1>
+        {/* para */}
+        <div className="text-gray-600 text-xs sm:text-sm">
+          Find your ideal home effortlessly with Rao Resident—your go-to
+          platform for quality rental properties and hassle-free living.
+          <br />
+          We have wide range of properties for you to choose from.
+        </div>
+        <Link className="" to={"/search"}>
+          <button className="bg-white py-2 px-5 font-semibold text-slate-800 rounded-lg text-center">
+            Explore now...
+          </button>
+        </Link>
+      </div>
+
+      {/* swiper */}
+
+      <Swiper navigation>
+        {offerListings &&
+          offerListings.length > 0 &&
+          offerListings.map((listing) => (
+            <SwiperSlide key={listing._id}>
+              <div
+                style={{
+                  background: `url(${listing.imageUrls[0]}) center no-repeat`,
+                  backgroundSize: "cover" , 
+                  boxShadow: "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset"
+                }}
+                className="h-[500px]"
+              ></div>
+            </SwiperSlide>
+          ))}
+      </Swiper>
+
+      {/* listing results for offer sale and rent */}
+
+      {/* OFFER LISTING- */}
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
+        {offerListings && offerListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent Offers
+              </h2>
+              <Link
+                to={"/search?offer=true"}
+                className="text-sm text-blue-800 hover:underline"
+              >
+                Show more offers
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {offerListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+
+        {/* RENT LISTING */}
+        {rentListings && rentListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent Places for Rent
+              </h2>
+              <Link
+                to={"/search?rent=true"}
+                className="text-sm text-blue-800 hover:underline"
+              >
+                Show more places for rent
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {rentListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+
+        {/* SALE LISTING */}
+        {saleListings && saleListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent Places For Sale
+              </h2>
+              <Link
+                to={"/search?sale=true"}
+                className="text-sm text-blue-800 hover:underline"
+              >
+                Show more places for sale
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {saleListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
