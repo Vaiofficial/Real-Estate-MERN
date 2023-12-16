cf73213fa78582dbcff34540cac53703b0325d09
P Vaibhav <pvaibhavofficial@gmail.com>
2023-12-16 11:08:20 +0530

    add onChange and OnSubmit functionality to the search Page

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -19,5 +19,5 @@ router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', getListing);
 router.get('/get', getListings);
 
- 
+
 export default router;
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,16 +1,129 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { json, useNavigate } from "react-router-dom";
 
 export default function Search() {
+  const navigate = useNavigate();
+  // search page mai jo search button hai.
+  //so initially value set kiye hai ab button click ke according url channge karna hoga.
+  const [sidebardata, setSidebardata] = useState({
+    searchTerm: "",
+    type: "all",
+    parking: false,
+    furnished: false,
+    offer: false,
+    sort: "created_at",
+    order: "desc",
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [Listing, setListing] = useState([]);
+  console.log(Listing);
+
+    //so jab url change hota hai , to search term dikhna chahaiye search bar mai , jo bhi search kiye the , or url change hoga to vo change ui mai bhi dikhega.
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    const typeFromUrl = urlParams.get("type");
+    const parkingFromUrl = urlParams.get("parking");
+    const furnishedFromUrl = urlParams.get("furnished");
+    const offerFromUrl = urlParams.get("offer");
+    const sortFromUrl = urlParams.get("sort");
+    const orderFromUrl = urlParams.get("order");
+
+    if (
+      searchTermFromUrl ||
+      typeFromUrl ||
+      parkingFromUrl ||
+      furnishedFromUrl ||
+      offerFromUrl ||
+      sortFromUrl ||
+      orderFromUrl
+    ) {
+      setSidebardata({
+        searchTerm: searchTermFromUrl || "",
+        type: typeFromUrl || "all",
+        parking: parkingFromUrl === "true" ? true : false,
+        furnished: furnishedFromUrl === "true" ? true : false,
+        offer: offerFromUrl === "true" ? true : false,
+        sort: sortFromUrl || "created_at",
+        order: orderFromUrl || "desc",
+      });
+    }
+
+    //ab jo bhi select & search kiye uske according ab result dena hoga.
+    const fetchListings = async () => {
+      setLoading(true);
+      const searchQuery = urlParams.toString();
+      const res = await fetch(`/api/listing/get?${searchQuery}`);
+      const data = await res.json();
+      setListing(data);
+      setLoading(false);
+    };
+
+    fetchListings();
+  }, [location.search]); //iska mtlab - jab location ka search hoga tab tab ye effect call hoga.
+
+  const handleChange = (e) => {
+    if (
+      e.target.id === "all" ||
+      e.target.id === "rent" ||
+      e.target.id === "sale"
+    ) {
+      setSidebardata({ ...sidebardata, type: e.target.id });
+    }
+
+    if (e.target.id === "searchTerm") {
+      setSidebardata({ ...sidebardata, searchTerm: e.target.value });
+    }
+
+    if (
+      e.target.id === "parking" ||
+      e.target.id === "furnished" ||
+      e.target.id === "offer"
+    ) {
+      setSidebardata({
+        ...sidebardata,
+        [e.target.id]:
+          e.target.checked || e.target.checked === "true" ? true : false,
+      });
+    }
+    if (e.target.id === "sort_order") {
+      const sort = e.target.value.split("_")[0] || "created_at";
+      const order = e.target.value.split("_")[1] || "desc";
+      setSidebardata({ ...sidebardata, sort, order });
+    }
+  };
+
+  //so search karne par url bhi change hoga and searching hogi.
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams();
+    urlParams.set("searchTerm", sidebardata.searchTerm);
+    urlParams.set("type", sidebardata.type);
+    urlParams.set("parking", sidebardata.parking);
+    urlParams.set("furnished", sidebardata.furnished);
+    urlParams.set("offer", sidebardata.offer);
+    urlParams.set("sort", sidebardata.sort);
+    urlParams.set("order", sidebardata.order);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
   return (
     <div className="flex flex-col md:flex-row">
       <div className="p-7 border-b-2 border-slate-400 md:border-r-2 md:min-h-screen">
-        <form className="flex flex-col gap-8">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-8">
           <div className="flex gap-3 items-center">
-            <label className="whitespace-nowrap font-semibold">Search Term:</label>
+            <label className="whitespace-nowrap font-semibold">
+              Search Term:
+            </label>
             <input
               type="text"
               className="p-3 rounded-md w-full border text-slate-600"
               placeholder="Search..."
+              id="searchTerm"
+              value={sidebardata.searchTerm}
+              onChange={handleChange}
             ></input>
           </div>
 
@@ -19,19 +132,43 @@ export default function Search() {
           <div className="flex gap-2 flex-wrap items-center">
             <label className="font-semibold">Type:</label>
             <div className="flex gap-2">
-              <input type="checkbox" id="all" className="w-5" />
+              <input
+                type="checkbox"
+                id="all"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebardata.type == "all"}
+              />
               <span>Rent & Sale</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="rent" className="w-5" />
+              <input
+                type="checkbox"
+                id="rent"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebardata.type === "rent"}
+              />
               <span>Rent</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="sale" className="w-5" />
+              <input
+                type="checkbox"
+                id="sale"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebardata.type === "sale"}
+              />
               <span>Sale</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="offer" className="w-5" />
+              <input
+                type="checkbox"
+                id="offer"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebardata.offer}
+              />
               <span>Offer</span>
             </div>
           </div>
@@ -41,11 +178,23 @@ export default function Search() {
           <div className="flex gap-2 flex-wrap items-center">
             <label className="font-semibold">Amenities:</label>
             <div className="flex gap-2">
-              <input type="checkbox" id="parking" className="w-5" />
+              <input
+                type="checkbox"
+                id="parking"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebardata.parking}
+              />
               <span>Parking</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="furnished" className="w-5" />
+              <input
+                type="checkbox"
+                id="furnished"
+                className="w-5"
+                onChange={handleChange}
+                checked={sidebardata.furnished}
+              />
               <span>Furnished</span>
             </div>
           </div>
@@ -54,11 +203,16 @@ export default function Search() {
 
           <div className="flex items-center gap-3">
             <label className="font-semibold">Sort:</label>
-            <select id="sort_order" className="p-2 rounded-lg border">
-                <option>Price high to low</option>
-                <option>Price low to high</option>
-                <option>Latest</option>
-                <option>Oldest</option>
+            <select
+              id="sort_order"
+              className="p-2 rounded-lg border"
+              onChange={handleChange}
+              defaultValue={"created_at_desc"}
+            >
+              <option value="regularPrice_desc">Price high to low</option>
+              <option value="regularPrice_asc">Price low to high</option>
+              <option value="createdAt_desc">Latest</option>
+              <option value="createdAt_asc">Oldest</option>
             </select>
           </div>
 
@@ -67,7 +221,6 @@ export default function Search() {
           <button className="p-3 text-white bg-slate-700 rounded-lg hover:opacity-95 uppercase font-semibold text">
             Search
           </button>
-
         </form>
       </div>
 
