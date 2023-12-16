36e4e6efbd189ce004ff15d3dc59cadf25fa8dba
P Vaibhav <pvaibhavofficial@gmail.com>
2023-12-16 21:19:43 +0530

    Completed About Page UI

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
 
-export default function About() {
+const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="max-w-4xl mx-auto my-8 p-6 bg-white rounded-md shadow-md">
+      <h1 className='font-bold text-3xl text-slate-800 mb-4'>About Rao Resident</h1>
+      <p className="mb-4 text-gray-700">
+        Welcome to Rao Resident, your premier destination for finding the perfect living space tailored to your unique lifestyle. At Rao Resident, we understand that a home is more than just a place; it's a reflection of your personality and aspirations. Whether you're in search of a cozy rental, a dream home for sale, or seeking the latest property offerings, we're here to simplify your journey in the realm of real estate.
+      </p>
+      
+      <h2 className='font-bold  text-lg text-slate-700'>Our Commitment</h2>
+      <p className="mb-4 text-gray-700">
+        Built on a foundation of trust and innovation, Rao Resident is committed to providing a seamless and delightful experience in your quest for the ideal home. Our platform brings together an extensive range of properties, from exclusive offers to homes for rent and sale, all curated to meet the diverse needs of our valued users. With a user-friendly interface and a commitment to transparency, we strive to redefine the way you discover and secure your next living space.
+      </p>
+
+      <h2 className='font-bold text-lg text-slate-700'>The Rao Resident Advantage</h2>
+      <p className="mb-8 text-gray-700">
+        What sets us apart is our unwavering dedication to making the process of finding a home not only efficient but enjoyable. Whether you're a first-time renter, a seasoned buyer, or someone exploring the market for lucrative offers, Rao Resident is your reliable partner. Join our community, explore our listings, and embark on a journey where your dream home is just a click away. At Rao Resident, we believe that everyone deserves a space that feels like home, and we're here to help you find yours.
+      </p>
+      {/* <h2 className='text-lg font-bold'>P Vaibhav</h2> */}
+      <h1 className='text-lg font-semibold text-orange-600'>Thankyou</h1>
+    </div>
+  );
+};
+
+export default About;
