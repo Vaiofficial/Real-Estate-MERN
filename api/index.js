3057f6b7d4a43d70a99dd81116bf86cc4fd7774c
P Vaibhav <pvaibhavofficial@gmail.com>
2023-12-17 11:32:51 +0530

    added pre installation fro deployment

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -13,6 +14,9 @@ mongoose.connect(process.env.MONGO).then(()=>{
 }).catch((err)=>{
     console.log(err);
 });
+
+
+const __dirname = path.resolve();
 
 const app  = express(); 
 
@@ -28,6 +32,15 @@ app.listen(3000 , ()=>{
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+
+//agar upar ke address mai nahi gya means client side jyga and index.html run karega.
+//dist name ka ak folder create hoga client side ke andar jo ki index.html ko store kar ke rakha hai.
+app.use(express.static(path.join(__dirname , '/client/dist')));
+
+app.get('*' , (req , res)=>{
+    res.sendFile(path.join(__dirname , 'client' , 'dist' , 'index.html'));
+})
 
 
 //MIDDLEWARE
