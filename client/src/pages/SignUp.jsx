76e0db3ec33464d9eac9072c7acc6bf70f38d7d3
P Vaibhav <pvaibhavofficial@gmail.com>
2023-12-17 12:18:19 +0530

    fix signin and signup file for OAuth

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import OAuth from "../components/OAuth.jsX";
+import OAuth from "../components/OAuth";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
